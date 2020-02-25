2c971dbb64ef2a918de26a5f284e039589babab3
Sai Teja Pratap <sai.teja.pratap@foo.bar>
2020-02-25 04:29:27 -0800

    Fix conda

--- bashrc.sh
+++ bashrc.sh
@@ -3,6 +3,33 @@ source /usr/local/etc/bash_completion.d/git-prompt.sh
 # Add custom bashrc here. Ignore the failure if the the custom script 
 # does not exist.
 
+# TODO(dotslash): Define more formatting helpers.
+# TODO(dotslash): Export this somehow
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+DULLYELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PINK='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHTGRAY='\033[0;37m'
+DARKGRAY='\033[1;30m'
+LIGHTRED='\033[1;31m'
+LIGHTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHTBLUE='\033[1;34m'
+LIGHTPINK='\033[1;35m'
+LIGHTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
+
+test_colors() {
+  echo -e "$RED RED $GREEN GREEN $DULLYELLOW DULLYELLOW $BLUE BLUE $PINK PINK"
+  echo -e "$CYAN CYAN $LIGHTGRAY LIGHTGRAY $DARKGRAY DARKGRAY $LIGHTRED LIGHTRED"
+  echo -e "$LIGHTGREEN LIGHTGREEN $YELLOW YELLOW $LIGHTBLUE LIGHTBLUE "
+  echo -e "$LIGHTPINK LIGHTPINK $LIGHTCYAN LIGHTCYAN $WHITE WHITE"
+  echo -e "$RED RED $NOCOLOR NOCOLOR"
+}
+
 # returns '(master)'
 parse_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
@@ -13,7 +40,7 @@ parse_git_branch() {
 get_time() { date +"%T"; }
 # PS1 with git info. Example:
 # >12:27:11 ~/dotfiles (master)$
-export PS1=">\$(get_time) \[\033[32m\]\w\[\033[33m\] \$(parse_git_branch)\[\033[00m\]$ "
+export PS1=">\$(get_time) ${GREEN}\w${YELLOW} \$(parse_git_branch)${NOCOLOR}$ "
 alias ls='ls -G'
 
 
@@ -66,28 +93,37 @@ alias sqlite3='sqlite3 -column -header'
 HISTTIMEFORMAT="%d/%m/%y %T "
 
 
-# added by Miniconda3 4.7.12 installer
-# >>> conda init >>>
-# !! Contents within this block are managed by 'conda init' !!
-
-export CONDA_CHANGEPS1=true
-# Use the conda env "bash". The idea is that I will install python cli utils in this
-# env without impacting the default conda env
-CONDA_ENV_TO_USE=bash
-__conda_setup="$(CONDA_REPORT_ERRORS=false '/Users/sai.suram/opt/miniconda3/bin/conda' shell.bash hook 2> /dev/null)"
-if [ $? -eq 0 ]; then
-   \eval "$__conda_setup"
-   CONDA_CHANGEPS1=true conda activate $CONDA_ENV_TO_USE
-else
-    if [ -f "/Users/sai.suram/opt/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/Users/sai.suram/opt/miniconda3/etc/profile.d/conda.sh"
-        CONDA_CHANGEPS1=true conda activate $CONDA_ENV_TO_USE
-    else
-        \export PATH="/Users/sai.suram/opt/miniconda3/bin:$PATH"
-    fi
+CONDA_BASE=""
+if [ -d "/opt/miniconda3" ]; then
+  CONDA_BASE="/opt/miniconda3"
+elif [ -d "~/opt/miniconda3"]; then
+  CONDA_BASE=~/opt/miniconda3
 fi
-unset __conda_setup
-# <<< conda init <<<
+
+if [[ CONDA_BASE ]]; then
+  # added by Miniconda3 4.7.12 installer (modified by me)
+  # >>> conda init >>>
+  # !! Contents within this block are managed by 'conda init' !!
+  export CONDA_CHANGEPS1=false
+  # Use the conda env "bash". The idea is that I will install python cli utils in this
+  # env without impacting the default conda env
+  CONDA_ENV_TO_USE=bash
+  __conda_setup="$(CONDA_REPORT_ERRORS=false "$CONDA_BASE/bin/conda" shell.bash hook 2> /dev/null)"
+  if [ $? -eq 0 ]; then
+     \eval "$__conda_setup"
+     conda activate $CONDA_ENV_TO_USE
+  else
+      if [ -f "$CONDA_BASE/etc/profile.d/conda.sh" ]; then
+          . "$CONDA_BASE/etc/profile.d/conda.sh"
+          conda activate $CONDA_ENV_TO_USE
+      else
+          \export PATH="$CONDA_BASE/bin:$PATH"
+      fi
+  fi
+  unset __conda_setup
+  # <<< conda init <<<
+  export PS1="${PINK}(\$(basename $CONDA_DEFAULT_ENV))${NOCOLOR} $PS1"
+fi
 
 # If there is a DOTFILES_GIT_REPO/custom_bashrc.sh, then that will be symlinked
 # to .custom_bashrc.sh. This is done to make it "easy" to modify the custom 
