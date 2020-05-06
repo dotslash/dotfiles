ca6de634af45d6fad65077ae941c8ec80f167124
Sai Teja Pratap <pts@avah.dev>
2020-05-06 00:35:06 -0700

    checkin

--- bashrc.sh
+++ bashrc.sh
@@ -5,6 +5,10 @@ source /usr/local/etc/bash_completion.d/git-prompt.sh
 
 # TODO(dotslash): Define more formatting helpers.
 # TODO(dotslash): Export this somehow
+# NOTE about non prining characters
+#  - https://stackoverflow.com/a/49679329/12110221
+#  - https://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/nonprintingchars.html
+
 NOCOLOR='\033[0m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -22,6 +26,24 @@ LIGHTPINK='\033[1;35m'
 LIGHTCYAN='\033[1;36m'
 WHITE='\033[1;37m'
 
+PS_NOCOLOR='\[\033[0m\]'
+PS_RED='\[\033[0;31m\]'
+PS_GREEN='\[\033[0;32m\]'
+PS_DULLYELLOW='\[\033[0;33m\]'
+PS_BLUE='\[\033[0;34m\]'
+PS_PINK='\[\033[0;35m\]'
+PS_CYAN='\[\033[0;36m\]'
+PS_LIGHTGRAY='\[\033[0;37m\]'
+PS_DARKGRAY='\[\033[1;30m\]'
+PS_LIGHTRED='\[\033[1;31m\]'
+PS_LIGHTGREEN='\[\033[1;32m\]'
+PS_YELLOW='\[\033[1;33m\]'
+PS_LIGHTBLUE='\[\033[1;34m\]'
+PS_LIGHTPINK='\[\033[1;35m\]'
+PS_LIGHTCYAN='\[\033[1;36m\]'
+PS_WHITE='\[\033[1;37m\]'
+
+# TODO: The colors are broken now. The \[non_print\] works only for bash prompt
 test_colors() {
   echo -e "$RED RED $GREEN GREEN $DULLYELLOW DULLYELLOW $BLUE BLUE $PINK PINK"
   echo -e "$CYAN CYAN $LIGHTGRAY LIGHTGRAY $DARKGRAY DARKGRAY $LIGHTRED LIGHTRED"
@@ -40,7 +62,7 @@ parse_git_branch() {
 get_time() { date +"%T"; }
 # PS1 with git info. Example:
 # >12:27:11 ~/dotfiles (master)$
-export PS1=">\$(get_time) ${GREEN}\w${YELLOW} \$(parse_git_branch)${NOCOLOR}$ "
+export PS1=">\$(get_time) ${PS_GREEN}\w${PS_YELLOW} \$(parse_git_branch)${PS_NOCOLOR}$ "
 alias ls='ls -G'
 
 
@@ -55,6 +77,10 @@ alias glog='git log -10'
 function swap() {
     local TMPFILE=tmp.$$;
     mv "$1" $TMPFILE && mv "$2" "$1" && mv $TMPFILE "$2";
+}
+# Reverse of vm
+function vm() { 
+    mv $2 $1;
 }
 
 # Example: git_find_replace foo bar
@@ -85,6 +111,17 @@ confirm() {
 # Example: urldecode 'foo.com/bar?id%3D184ff84d27c3613d%26quality%3Dmedium'
 # Expects python3
 alias urldecode='python -c "import urllib.parse, sys; print(urllib.parse.unquote(sys.argv[1] if len(sys.argv) > 1 else sys.stdin.read()[0:-1]))"'
+# $ cat /tmp/json
+# [
+#  {"name":"val1", "something_else": "something2"},
+#  {"name":"val2"}
+# ]
+# $ json_tabulate < /tmp/json
+# name    something_else
+# ------  ----------------
+# val1    something2
+# val2
+alias json_tabulate='python -c "from colored import fg, bg, attr; import json; import fileinput; import tabulate; lines=[line for line in fileinput.input() if line.strip()]; dicts=json.loads(\" \".join(lines)); keys = (dicts and dicts[0].keys()) or []; keys = {k:fg(\"yellow\") + attr(\"bold\")+ k + attr(\"reset\") for k in keys}; print(tabulate.tabulate(dicts, headers=keys))"'
 
 # https://github.com/dotslash/recent2: Logs bash history to an sqlite file
 export PROMPT_COMMAND='log-recent -r $? -c "$(HISTTIMEFORMAT= history 1)" -p $$'
@@ -134,7 +171,7 @@ if [[ $CONDA_BASE ]]; then
   fi
   unset __conda_setup
   # <<< conda init <<<
-  export PS1="${PINK}(\$(get_conda_env))${NOCOLOR} $PS1"
+  export PS1="${PS_PINK}(\$(get_conda_env))${PS_NOCOLOR} $PS1"
 fi
 
 # If there is a DOTFILES_GIT_REPO/custom_bashrc.sh, then that will be symlinked
